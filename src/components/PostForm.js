7f460415b0973c8cb366bbb1718af1fb1944bcb4
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-04-21 10:51:44 -0400

    added a form, we need to add the post request

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -5,8 +5,14 @@ class PostForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-        
-        }
+        title: '',
+        body: ''
+        };
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({[e.target.name]: e.target.value})
     }
 
     render() {
@@ -14,11 +20,19 @@ class PostForm extends Component {
             <div>
                 <h1>did this work</h1>
                 <form>
-                <div>
-                <label>
-                
-                </label>
-                </div>
+                    <div>
+                        <label>Title:</label>
+                        <br />
+                        <input type="text" name="title" onChange={this.onChange} value={this.state.title} />
+                        </div>
+                        <br />
+                        <div>
+                        <label>Body:</label>
+                        <br />
+                        <textarea name="body" onChange={this.onChange} value={this.state.body} />
+                    </div>
+                        <br />
+                        <button type="submit">Submit</button>
                 </form>
             </div>
         )
