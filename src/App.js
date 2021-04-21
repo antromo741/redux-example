7fbf8e486483df2dd550db777d2154c71c56b018
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-04-21 17:19:30 -0400

    adding store and added reducer folder with index.js in it

--- src/App.js
+++ src/App.js
@@ -3,26 +3,24 @@ import './App.css';
 
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
+import { Provider } from 'react-redux'
+import store from './store';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Hi, Hello, What it do?</h1>
-         
-        
-      
-      </header>
-
-      <PostForm />
-
-      <hr />
-      
-      <Posts />
-
-    </div>
+class App extends Component {
+   render() {
+     return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Hi, Hello, What it do?</h1>
+          </header>
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
   );
 }
-
+}
 export default App;
