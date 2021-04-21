d8849aa2d34c89a628d86a5e42233ccf476e37c2
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-04-21 15:55:56 -0400

    added the onSubmit

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -9,17 +9,36 @@ class PostForm extends Component {
         body: ''
         };
         this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     onChange(e) {
         this.setState({[e.target.name]: e.target.value})
     }
 
+    onSubmit(e) {
+        e.preventDefault();
+
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        }
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+
     render() {
         return (
             <div>
-                <h1>did this work</h1>
-                <form>
+                <h1>Add a post</h1>
+                <form onSubmit={this.onSubmit}>
                     <div>
                         <label>Title:</label>
                         <br />
