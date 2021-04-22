e119c0753f17e5831c22f41800dea7295ae4c7bb
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-04-22 17:03:37 -0400

    added a post reducer, added to the root reducer. using combinereducer,now defining actions as well as setting up the next reducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
+
 import { Provider } from 'react-redux'
 import store from './store';
 
