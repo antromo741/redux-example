1b6f1a5bc73fbf2fcbb212436ad2a47102bcabdb
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-04-22 18:00:49 -0400

    added to posts, now going to add redux dev tools

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { fetchPosts } from '../actions/postActions';
 
 class Posts extends Component {
     
     //No longer need component will mount or constructor, we are getting our state from redux, the store
-    
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
     render() {
 
-        const postItems = this.state.posts.map(post => (
+        const postItems = this.props.posts.map(post => (
             <div key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.body}</p>
@@ -21,5 +26,15 @@ class Posts extends Component {
         )
     }
 }
+Posts.propTypes = {
+    fetchPosts: PropTypes.func.isRequired,
+    posts: PropTypes.array.isRequired
+};
 
-export default Posts
+
+//Using posts because in the root reducer we declare posts
+const mapStateToProps = state => ({
+    posts: state.posts.items
+})
+
+export default connect(mapStateToProps, {fetchPosts})(Posts)
